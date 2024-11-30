ae5f54219e7f2902ddd1a4e4f72d755358f3c99c
Derekbbq <861454053@qq.com>
2024-11-30 14:35:14 +0700

    Initial commit

---
+++ test.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HelloWorld {
+    string strVar = "Web3 hello";
+
+    function sayHello() public view returns (string memory) {
+        return  addInfo(strVar);
+    }
+    function setHello(string memory newstring)public {
+        strVar = newstring;
+    }
+    function addInfo(string memory addString)internal pure returns(string memory){
+        return string.concat(addString,"from Henry's contract.");
+    }
+}
+
+
