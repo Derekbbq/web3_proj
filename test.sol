a89a45a28b77bb90f2eee4df0b78c9a3bd9f926b
Derekbbq <112763861+Derekbbq@users.noreply.github.com>
2024-11-30 15:39:16 +0700

    Update test.sol

--- test.sol
+++ test.sol
@@ -10,7 +10,7 @@ contract HelloWorld {
     function setHello(string memory newstring)public {
         strVar = newstring;
     }
-    function addInfo(string memory addString)internal pure returns(string memory){
+    function addInfo (string memory addString)internal pure returns(string memory){
         return string.concat(addString,"from Henry's contract.");
     }
 }
